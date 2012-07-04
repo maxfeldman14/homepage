b79b5a330d02f6e3ce9cc151c7cb7c4eae71ca7a
Max <maxfeldman14@gmail.com>
2012-07-04 15:23:49 -0700

    Formatted projects, temporarily removed pix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,10 @@ class HomeController < ApplicationController
     @projs = [
       { name: "Bro", description: 
         "I am currently working on ARP spoofing detection using [bro], a powerful network traffic analysis tool.
-[bro]: http://www.bro-ids.org/", pic: "bro.png"},
+
+[github]
+[bro]: http://www.bro-ids.org/
+[github]: https://github.com/maxfeldman14/brospects", pic: "bro.png"},
       { name: "MERmaid", description: 
         "I have recently begun contributing to [MERmaid], a distributed *de novo* genome assembler.
 [MERmaid]: https://github.com/mermaid-assembler", pic: "mermaid.png"}
@@ -13,8 +16,11 @@ class HomeController < ApplicationController
     @pasts = [
       { name: "PHELONY", description: 
         "In the Spring 2012 semester I took [CS194-22]: Cell Phones as a Computing Platform. Our team contributed to the [OsmocomBB] project, an open source GSM Baseband software implementation.
+
+[github]
 [CS194-22]: http://phone.cs.berkeley.edu/dokuwiki/doku.php?id=194-22:sp2012
-[OsmocomBB]: http://bb.osmocom.org/", pic: "osmocom.png"}
+[OsmocomBB]: http://bb.osmocom.org/
+[github]: https://github.com/maxfeldman14/PHELONY", pic: "osmocom.png"}
     ]
   end
 
