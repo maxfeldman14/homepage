58622c3ce010bbbbb8caa6e0b979558cab3fc9d9
Max <maxfeldman14@gmail.com>
2012-07-08 01:56:51 -0700

    Testing using Twilio GET instead of POST

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,5 @@
 class TwilioController < ApplicationController
-  protect_from_forgery :except => :create 
+  #protect_from_forgery :except => :create 
 
   def index
     # A post will end up here; fetch the Body and From
